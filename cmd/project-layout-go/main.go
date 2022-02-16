c50c10ed60f7fb2964cc8124fe21f03d9410fa6e
MisterChing <chfnaruto@gmail.com>
2022-02-16 19:37:15 +0800

    add files

--- cmd/project-layout-go/main.go
+++ cmd/project-layout-go/main.go
@@ -1,60 +1,60 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "log"
-    "net/http"
-    "project-layout-go/internal/infrastructure/bootstrap"
-    "project-layout-go/internal/infrastructure/common/cache"
-    "project-layout-go/internal/infrastructure/common/config"
-    "project-layout-go/internal/infrastructure/common/db"
-    "project-layout-go/internal/router"
-    "project-layout-go/pkg/osx"
-    "project-layout-go/pkg/utils/debugutil"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"project-layout-go/internal/infrastructure/bootstrap"
+	"project-layout-go/internal/infrastructure/common/cache"
+	"project-layout-go/internal/infrastructure/common/config"
+	"project-layout-go/internal/infrastructure/common/db"
+	"project-layout-go/internal/router"
+	"project-layout-go/pkg/osx"
+	"project-layout-go/pkg/utils/debugutil"
 )
 
 func main() {
-    // bootloader
-    bootloader := bootstrap.NewBootStrap()
+	// bootloader
+	bootloader := bootstrap.NewBootStrap()
 
-    bootloader.AddBeforeServerFunc(
-        config.SetUp(),
-        //logger.SetUp(),
-        db.SetUp(),
-        cache.SetUp(),
-    )
-    bootloader.AddAfterServerFunc(
-        db.Destroy(),
-        cache.Destroy(),
-    )
-    if err := bootloader.SetUp(); err != nil {
-        log.Fatal(err)
-    }
-    defer bootloader.Destroy()
+	bootloader.AddBeforeServerFunc(
+		config.SetUp(),
+		//logger.SetUp(),
+		db.SetUp(),
+		cache.SetUp(),
+	)
+	bootloader.AddAfterServerFunc(
+		db.Destroy(),
+		cache.Destroy(),
+	)
+	if err := bootloader.SetUp(); err != nil {
+		log.Fatal(err)
+	}
+	defer bootloader.Destroy()
 
-    // 设置进程最大打开文件描述符个数
-    osx.SetRLimit()
+	// 设置进程最大打开文件描述符个数
+	osx.SetRLimit()
 
-    // set gin mode
-    if config.Env == config.GrayEnv || config.Env == config.ProductEnv {
-        gin.SetMode(gin.ReleaseMode)
-    } else {
-        gin.SetMode(gin.DebugMode)
-    }
-    engine := gin.Default()
+	// set gin mode
+	if config.Env == config.GrayEnv || config.Env == config.ProductEnv {
+		gin.SetMode(gin.ReleaseMode)
+	} else {
+		gin.SetMode(gin.DebugMode)
+	}
+	engine := gin.Default()
 
-    router.Init(engine)
+	router.Init(engine)
 
-    // Listen and Server in 0.0.0.0:8080
-    serverAddr := config.Addr
-    srv := &http.Server{
-        Addr:    serverAddr,
-        Handler: engine,
-    }
-    debugutil.DebugPrint(srv, 0)
+	// Listen and Server in 0.0.0.0:8080
+	serverAddr := config.Addr
+	srv := &http.Server{
+		Addr:    serverAddr,
+		Handler: engine,
+	}
+	debugutil.DebugPrint(srv, 0)
 
-    if err := srv.ListenAndServe(); err != nil {
-        log.Fatal(err)
-    }
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 
 }
--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/prometheus/client_golang v1.12.1
 	github.com/spf13/cast v1.4.1
 	github.com/spf13/viper v1.10.1
+	go.uber.org/zap v1.21.0
 )
 
 require (
@@ -51,6 +52,8 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	golang.org/x/text v0.3.7 // indirect
